904e7335c7fab05ee8359bc5efe124078fb4b57c
yef2015 <522142978@qq.com>
2015-08-08 14:28:43 +0800

    create

---
+++ WebApplication27ForGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication27ForGit.Startup))]
+namespace WebApplication27ForGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
